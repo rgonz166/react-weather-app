4b0aff2d514e7d599177b2750b48942b4f918b91
Raul Gonzalez <rgonz166@gmail.com>
2022-05-07 04:52:39 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const city = 'holtville';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfa67be28c113176d4ae11eb2135395a`;
+
   return (
-    <div className="App">
+    <div className="app">
+
     </div>
   );
 }
